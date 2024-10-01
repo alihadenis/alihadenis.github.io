3ad6f00007a39570fd955b099833311821489fe5
alihadens <denisalishaputra@gmail.com>
2024-10-02 00:12:32 +0700

    add banyak

--- script/script.js
+++ script/script.js
@@ -1,6 +1,10 @@
-const menuIcon=document.getElementById('menu-icon');
-const menuSamping=document.getElementById('menu-samping');
+const bar=document.querySelector('.barr');
+const menuSamping=document.querySelector('.menu-samping');
 
-menuIcon.addEventListener('click', () => {
-    menuSamping.classList.toggle('show');
+//membuat even saat bar di click menu samping akan muncul di atas layar
+
+bar.addEventListener('click', () => {
+  menuSamping.classList.toggle('show')
 });
+
+//membuat even saat menu samping di click menu samping akan dimatikan
